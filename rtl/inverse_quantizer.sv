93cdaa5b47c30f8816c997dc5bc0e6b88b7742ce
Davit Markarian <davit@udxs.me>
2022-05-01 20:22:06 -0700

    fix diffq extension

--- rtl/inverse_quantizer.sv
+++ rtl/inverse_quantizer.sv
@@ -5,7 +5,7 @@ module inverse_quantizer(
 	output logic [15:0] predicted);
 
 
-	wire [15:0] staged_diffq [4];    
+	wire [18:0] staged_diffq [4];    
 	assign staged_diffq[0] = step_size >> 3;
 	for(genvar i = 1; i <= 3; i = i + 1) begin
 		assign staged_diffq[i] = code[i - 1] ? staged_diffq[i - 1] + (step_size >> (i - 1)) : staged_diffq[i - 1];
@@ -13,13 +13,11 @@ module inverse_quantizer(
 
 
 	wire  [18:0] sample_ext;
-	wire  [18:0] diffq_ext;
 	assign sample_ext = { {3{prev_predicted[15]}}, prev_predicted};
-	assign diffq_ext = {3'b0, staged_diffq[3]};
 
 	wire  [18:0] predicted_ext;
 
-	assign predicted_ext = code[3] ? sample_ext - diffq_ext : sample_ext + diffq_ext;
+	assign predicted_ext = code[3] ? sample_ext - staged_diffq[3] : sample_ext + staged_diffq[3];
 
 	always_comb begin
 		if(| predicted_ext[19:16]) begin
