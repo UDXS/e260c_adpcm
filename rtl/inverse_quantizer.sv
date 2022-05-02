6afac45f9fbffefef0ca23299189d1a25761e172
Davit Markarian <davit@udxs.me>
2022-05-01 21:08:30 -0700

    Fix

--- rtl/inverse_quantizer.sv
+++ rtl/inverse_quantizer.sv
@@ -15,18 +15,17 @@ module inverse_quantizer(
 	wire  [18:0] sample_ext;
 	assign sample_ext = { {3{prev_predicted[15]}}, prev_predicted};
 
-	wire  [18:0] predicted_ext;
+	wire signed [18:0] predicted_ext;
 
 	assign predicted_ext = code[3] ? sample_ext - staged_diffq[3] : sample_ext + staged_diffq[3];
 
 	always_comb begin
-		if(predicted_ext > 0x7FFF)
-			predicted = 0x7FFF;
-		else if(-predicted_ext > 0x7FFF)
-			predicted = 0x8000;
+		if (predicted_ext > 32767)
+			predicted = -32767;
+		else if (predicted_ext < -32768)
+			predicted = -32768;
 		else
 			predicted = predicted_ext[15:0];
-
 	end
 
 endmodule
