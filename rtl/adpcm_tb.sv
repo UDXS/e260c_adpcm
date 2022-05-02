8652cedf8fb01e076ff21829d5e9390882c5fccc
Davit Markarian <davit@udxs.me>
2022-05-01 19:06:05 -0700

    Add Testbench

---
+++ rtl/adpcm_tb.sv
@@ -0,0 +1,49 @@
+module adpcm_tb();
+
+	reg clk;
+	reg reset;
+
+	reg [15:0] sample;
+	wire [3:0] code;
+
+    encoder enc_i(
+    .clk(clk),
+	.reset(reset),
+	.sample(sample),
+	.code(code));
+
+	wire [15:0] predsample;
+
+    decoder dec_i(
+	.clk(clk),
+	.reset(reset),
+	.code(code),
+	.predsample(predsample));
+
+	reg [7:0] wave_tab [256];
+	integer i;
+
+	initial begin
+		$display ("ADPCM Benchmark by D. Markarian");
+		$readmemh ("dat/wave.dat", wave_tab);
+
+		#1 clk = 1;
+		#1 clk = 0;
+		reset = 1;
+		#1 clk = 1;
+		#1 clk = 0;
+		reset = 0;
+
+		for(i = 0; i < 256; i++) begin
+			sample = {wave_tab[i], wave_tab[i]};
+			#1 clk = 1;
+			#1 clk = 0;
+			$display("i = %d s = 0x%X c(s) = 0x%X p(c(s))= 0x%X, ", i, sample, code, predsample);
+		end
+
+
+	end
+
+
+
+endmodule
---
+++ run_test.sh
@@ -0,0 +1,10 @@
+xrun \
++gui \
++access+rwc \
++xm64bit \
++xmstatus \
++xmtimescale+1ns/1ps \
++xmoveride_timescale \
+-v rtl/quantizer.sv \
+-v rtl/inquantizer.sv \
+rtl/fib_tb.sv
